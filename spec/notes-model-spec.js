a7cc5d0020fa762663101add7a4057ae4a488e8c
Ben <ben.lynch1931@gmail.com>
2020-11-10 11:57:35 +0000

    Re-structure files/directory

--- spec/notes-model-spec.js
+++ spec/notes-model-spec.js
@@ -1,35 +1,8 @@
-// (function(exports) {
-//   function testCircleRadiusDefaultsTo10() {
-//     var circle = new Circle();
-//
-//     if (circle.radius !== 10) {
-//       throw new Error("Circle size is not 10");
-//     }
-//   };
-//
-//   testCircleRadiusDefaultsTo10();
-// })(this);
-
-// (function(exports) {
-//
-//   function testShowMessage() {
-//     var note = new Note("Mace is the 'G'!");
-//
-//     if (note.showMessage() != "Mace is the 'G'!") {
-//       throw new Error("test showMessage() - FAILED")
-//     } else {
-//       console.log("test showMessage() - passed")
-//     }
-//   }
-//
-//   testShowMessage();
-// })(this);
-
-
-
 ( () => {
+
   test.that('The message is the same', () => {
-    let note = new Note("I am with mace!");
+    let note = new Note("I am with Mace!");
     expect.toEqual(note.showMessage(), "I am with Mace!");
+
   })
 })(this);
