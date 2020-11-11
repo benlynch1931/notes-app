c0e9a5d3c7d1be5faf711664182b83c741ad93e6
Ben <ben.lynch1931@gmail.com>
2020-11-11 10:28:42 +0000

    Feat: add function to display note in preview

--- src/notes-controller.js
+++ src/notes-controller.js
@@ -1,8 +1,16 @@
 let noteListView = new NoteListView();
 let notes = new Notes();
 notes.addNote("'Ruby sucks' - Mace");
-noteListHTML = noteListView.buildHTML(notes);
+notes.addNote("'Ruby is cool' - Ben");
+let noteListHTML = noteListView.buildHTML(notes);
 
-let appDiv = document.getElementById('app')
+let appDiv = document.getElementById('app');
 
 appDiv.innerHTML = noteListHTML;
+
+
+function viewNote(noteID) {
+  let noteView = SingleNoteView(notes.noteList[noteID-1]);
+  let previewDiv = document.getElementById('preview');
+  preiewDiv.innerHTML = noteView.buildHTML();
+}
